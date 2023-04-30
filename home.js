e9b5b2f030bb7ac700107f3cd580f12c73a6754e
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-30 18:52:29 -0500

    3 files modified, 1 added

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 // Get the SVG element
-let cursor = document.getElementById("cursor");
+let cursor = document.getElementById('cursor');
 
 function isTouchDevice() {
   return (
-    "ontouchstart" in window ||
+    'ontouchstart' in window ||
     navigator.maxTouchPoints > 0 ||
     navigator.msMaxTouchPoints > 0
   );
@@ -14,12 +14,12 @@ const move = (e) => {
     var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
     var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
   } catch (e) {}
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
 };
 
 // Attach a mousemove event listener to the document
-document.addEventListener("mousemove", (e) => {
+document.addEventListener('mousemove', (e) => {
   // Set the SVG's position to the mouse's x and y coordinates
   move(e);
 });
