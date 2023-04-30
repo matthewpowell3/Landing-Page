1abcd661a848abc6e1006bf606a9b429294fbcb7
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-29 20:19:12 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-
 // Get the SVG element
 let cursor = document.getElementById('cursor');
 
 function isTouchDevice() {
-try {
-  document.createEvent("TouchEvent");
-  return true;
-} catch (e) {
-  return false;
-}
+  return (
+    'ontouchstart' in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
 }
 
 const move = (e) => {
   try {
-    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX; 
-    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY; 
-} catch(e) {}
-   cursor.style.left = x + 'px';
-   cursor.style.top = y + 'px';
-} 
+    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+  } catch (e) {}
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+};
 
 // Attach a mousemove event listener to the document
 document.addEventListener('mousemove', (e) => {
   // Set the SVG's position to the mouse's x and y coordinates
- move(e);
+  move(e);
+});
+
+let header = document.querySelector('.header'); // Get the header element
+
+header.addEventListener('mouseover', function () {
+  cursor.classList.add('hide'); // Toggle the 'hide' class on the cursor element
+  console.log('over');
 });
