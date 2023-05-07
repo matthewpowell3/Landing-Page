70ef6c0cfc604ba7aea646ac600da29ac61a15ab
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-05-07 15:01:48 -0500

    3 files modified, 1 deleted

--- transition.js
+++
@@ -1,8 +0,0 @@
-function bubbleTransition() {
-  const container = document.querySelector('.container');
-  container.classList.add('bubble');
-
-  setTimeout(() => {
-    window.location.href = 'page2.html';
-  }, 1000);
-}
